21cfa9716cb9deb86f3515869ec32f2fe5350e28
0oaq <otbestimt@students.wcpss.net>
2022-03-07 18:44:25 +0000

    e

--- Index.js
+++
@@ -1,8 +0,0 @@
-let myName;
-let myAge;
-myAge = 21
-myName = 'chris'
-const string = 'this is my string';
-const browserType = 'mozilla';
-browserType.length;
-
---
+++ index.js
@@ -0,0 +1,10 @@
+let myName;
+let myAge;
+myAge = 21
+myName = 'chris'
+const string = 'this is my string';
+const browserType = 'mozilla';
+console.log(browserType.length);
+console.log(browserType[0]);
+browserType[browserType.length-1];
+console.log(browserType[browserType.length-1]);
