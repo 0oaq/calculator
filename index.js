c0b0e83acb5a88a0b63f728adb3138da4080894d
0oaq <otbestimt@students.wcpss.net>
2022-03-22 14:15:27 +0000

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const shopping = [12*[6/3]];
+console.log(shopping);
+const calc = [12, '+', 3, '*', 4]
+if (calc.includes('+')){
+    calc.shift()
+    calc.shift()
+calc.push('+', 12);
+}
+console.log(calc);
+for(const calcs of calc);
+console.log(calc);
