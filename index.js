e23945939b2fd82adc0df76fe7bd3c9d37879b76
0oaq <otbestimt@students.wcpss.net>
2022-03-22 17:55:22 +0000

    commit

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
-const shopping = [12*[6/3]];
+const shopping = [12 * [6 / 3]];
 console.log(shopping);
 const calc = [12, '+', 3, '*', 4]
-if (calc.includes('+')){
+if (calc.includes('+')) {
     calc.shift()
     calc.shift()
-calc.push('+', 12);
+    calc.push('+', 12);
 }
 console.log(calc);
-for(const calcs of calc);
+for (const calcs of calc);
 console.log(calc);
 document.getElementById("num:1").addEventListener("click", () => {
     document.getElementById("numberBox").value += "1"
+})
+document.getElementById("num:2").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "2"
+})
+document.getElementById("num:3").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "3"
+})
+document.getElementById("num:4").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "4"
+})
+document.getElementById("num:5").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "5"
+})
+document.getElementById("num:6").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "6"
+})
+document.getElementById("num:7").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "7"
+})
+document.getElementById("num:8").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "8"
+})
+document.getElementById("num:9").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "9"
+})
+document.getElementById("num:0").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "0"
+})
+document.getElementById("operation:+").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "+"
+})
+document.getElementById("operation:-").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "-"
+})
+document.getElementById("operation:/").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "/"
+})
+document.getElementById("operation:*").addEventListener("click", () => {
+    document.getElementById("numberBox").value += "*"
 })
