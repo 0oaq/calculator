8c67ccae1d8b3645a8e9a8fa16c0bdc7fb17dee0
0oaq <otbestimt@students.wcpss.net>
2022-03-24 14:47:12 +0000

    commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const shopping = [12 * [6 / 3]];
-console.log(shopping);
+"use strict"
 const calc = [12, '+', 3, '*', 4]
 if (calc.includes('+')) {
     calc.shift()
@@ -7,50 +6,9 @@ if (calc.includes('+')) {
     calc.push('+', 12);
 }
 console.log(calc);
-for (const calcs of calc);
+for (const calcs of calc) {};
 console.log(calc);
-document.getElementById("num:1").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "1"
-})
-document.getElementById("num:2").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "2"
-})
-document.getElementById("num:3").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "3"
-})
-document.getElementById("num:4").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "4"
-})
-document.getElementById("num:5").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "5"
-})
-document.getElementById("num:6").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "6"
-})
-document.getElementById("num:7").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "7"
-})
-document.getElementById("num:8").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "8"
-})
-document.getElementById("num:9").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "9"
-})
-document.getElementById("num:0").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "0"
-})
-document.getElementById("operation:+").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "+"
-})
-document.getElementById("operation:-").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "-"
-})
-document.getElementById("operation:/").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "/"
-})
-document.getElementById("operation:*").addEventListener("click", () => {
-    document.getElementById("numberBox").value += "*"
-})
+
 const addition = 0;
 const subtraction = 1;
 const multiplication = 2;
@@ -68,4 +26,48 @@ importance[subtraction] = 0
 importance[multiplication] = 1
 importance[division] = 1
 
-function 
+const operands = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let storage = [];
+let operatoradded = false
+
+const numberbox = document.getElementById("numberBox")
+
+function onNumberClick(number) {
+    if (operatoradded) {
+        operatoradded = false
+        numberbox.value = ""
+    }
+    storage.push(number)
+    numberbox.value += number;
+}
+
+let _operator = ""
+
+function onOperatorClick(operator) {
+    storage.push(operator)
+    operatoradded = true;
+}
+
+function setOperator(operator) {
+    _operator = operator
+}
+
+function onEnterClick(enter) {
+    let newarray = storage.join("")
+   
+    if (newarray.split(_operator)[1]) {
+        if (_operator === "+"){
+            numberbox.value = parseFloat(newarray.split(_operator)[0]) + parseFloat(newarray.split(_operator)[1])
+        } else if (_operator === "-"){
+            numberbox.value = parseFloat(newarray.split(_operator)[0]) - parseFloat(newarray.split(_operator)[1])
+        } else if (_operator === "*"){
+            numberbox.value = parseFloat(newarray.split(_operator)[0]) * parseFloat(newarray.split(_operator)[1])
+        } else if (_operator === "/"){
+            numberbox.value = parseFloat(newarray.split(_operator)[0]) / parseFloat(newarray.split(_operator)[1])
+        }
+    }
+}
+
+function onClearClick(clear) {
+    numberbox.value = ""
+}
