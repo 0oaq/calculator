ebd8e7237052e791281d9e48ace3d4126f7389c2
0oaq <otbestimt@students.wcpss.net>
2022-03-24 20:42:45 +0000

    commit

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ importance[division] = 1
 
 const operands = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 let storage = [];
-let operatoradded = false
+let operatorAdded = false
 
 const numberbox = document.getElementById("numberBox")
 
 function onNumberClick(number) {
-    if (operatoradded) {
-        operatoradded = false
+    if (operatorAdded) {
+        operatorAdded = false
         numberbox.value = ""
     }
     storage.push(number)
@@ -56,8 +56,10 @@ function onEnterClick(enter) {
     let newarray = storage.join("")
    
     if (newarray.split(_operator)[1]) {
+        let operandLeft = parseFloat(newarray.split(_operator)[0])
+        let operandRight = parseFloat(newarray.split(_operator)[1])
         if (_operator === "+"){
-            numberbox.value = parseFloat(newarray.split(_operator)[0]) + parseFloat(newarray.split(_operator)[1])
+            numberbox.value = operandLeft + operandRight
         } else if (_operator === "-"){
             numberbox.value = parseFloat(newarray.split(_operator)[0]) - parseFloat(newarray.split(_operator)[1])
         } else if (_operator === "*"){
